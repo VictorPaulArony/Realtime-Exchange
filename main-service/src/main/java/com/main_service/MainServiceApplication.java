230fb5e078357d5efe4c51b48d5211b53ac8bd0f
victorpaularony <smallypauls@gmail.com>
2025-05-13 21:16:00 +0300

    the main service application class for the main service

---
+++ main-service/src/main/java/com/main_service/MainServiceApplication.java
@@ -0,0 +1,14 @@
+package com.main_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//the main service application class for the main service 
+@SpringBootApplication
+public class MainServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainServiceApplication.class, args);
+	}
+
+}
