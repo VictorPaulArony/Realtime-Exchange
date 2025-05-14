89774e94e31db3cdad57c8f81324c537eb6ef2d1
victorpaularony <smallypauls@gmail.com>
2025-05-15 02:49:13 +0300

    Add documentation for RateServiceApplicationTests class

---
+++ rate-service/src/test/java/com/rate_service/RateServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.rate_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+// RateServiceApplicationTests class to test the application
+@SpringBootTest
+class RateServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
