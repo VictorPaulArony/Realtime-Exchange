34884d97613edbbd619b7002620581a6ad9c0bc8
victorpaularony <smallypauls@gmail.com>
2025-05-15 02:48:37 +0300

    Add documentation for RateServiceApplication class

---
+++ rate-service/src/main/java/com/rate_service/RateServiceApplication.java
@@ -0,0 +1,14 @@
+package com.rate_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// RateServiceApplication class to run the application
+@SpringBootApplication
+public class RateServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RateServiceApplication.class, args);
+	}
+
+}
